8a59cf6c700ca847ca245c6434eeab2a25fd34da
candy <cnandy114514@outlook.com>
2020-12-18 17:41:51 +0800

    feat: add nature to bible

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,14 @@ import bible from "../config/bible"
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container space-y-1">
       {bible.map(({ id, title, template }) => {
         return (
           <Quote
-            snippetId="support"
-            title="支援"
-            template={
-              "${支援者|桐生會長}，請您支援 #${被支援者|桐生ココ} ，她僅僅因為說${被支援者的主张|台灣是個國家}，就受${被支援者的反对者|中國五毛}機器人的攻擊以及人身威脅，和未婚夫${无关者|肖戰}的婚約也被迫取消，請一定聲援她！"
-            }
+            key={id}
+            snippetId={id}
+            title={title}
+            template={template}
           ></Quote>
         )
       })}
